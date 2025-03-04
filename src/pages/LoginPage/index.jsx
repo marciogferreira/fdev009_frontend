0e5b8263b07b1829665b6be18ba3035d1c225092
Geovane <dartanha272@gmal.com>
2025-03-03 21:39:08 -0300

    Realizado a validação e altenticação do usuario no login

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,25 +1,30 @@
-import imgCar from './img/img_car.png';
+import imgCar from "./img/img_car.png";
 
 export default function LoginPage() {
-    return (
-        <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
       <div className="login-container bg-warning p-4 rounded shadow text-center">
         {/* Ícone do Login */}
-        <img
-          src={imgCar}
-          alt="Login Icon"
-          className="mb-3"
-          width="90"
-        />
+        <img src={imgCar} alt="Login Icon" className="mb-3" width="90" />
         <h3 className="text-dark">Login Page</h3>
 
         {/* Formulário */}
         <form>
           <div className="mb-3">
-            <input type="text" className="form-control" placeholder="Usuário" required />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Usuário"
+              required
+            />
           </div>
           <div className="mb-3">
-            <input type="password" className="form-control" placeholder="Senha" required />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Senha"
+              required
+            />
           </div>
           <button type="submit" className="btn btn-dark w-100">
             Login
@@ -37,4 +42,15 @@ export default function LoginPage() {
       </div>
     </div>
   );
-};
+}
+/* validando usuario e encaminhando para a pagina painelPage */
+function logar() {
+  const usuario = document.getElementById("usuario").value;
+  const senha = document.getElementById("senha").value;
+  if (usuario === "admin" && senha === "123") {
+    window.location.href = "../painelPage";
+  } else {
+    alert("Usuário ou senha inválidos");
+  }
+  return false;
+}
