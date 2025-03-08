a08db7452f355ea121b840a3ab4e3118d9c0cbad
Geovane <dartanha272@gmal.com>
2025-03-08 15:23:49 -0300

    Criado a pagina Pedidos

--- src/layouts/Menu.jsx
+++ src/layouts/Menu.jsx
@@ -1,32 +1,51 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function Menu() {
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">IWStore</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link to="/"  className="nav-link active">Painel</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/categorias"  className="nav-link active">Categorias</Link>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" >Sair</a>
-                        </li>
-                    </ul>
-                    <div>
-                        <button className='btn btn-success btn-sm'>Doações</button>
-                    </div>
-                    </div>
-                </div>
-            </nav>   
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            IWStore
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link active">
+                  Painel
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/categorias" className="nav-link active">
+                  Categorias
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/pedidos" className="nav-link active">
+                  pedidos
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Sair</a>
+              </li>
+            </ul>
+            <div>
+              <button className="btn btn-success btn-sm">Doações</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
