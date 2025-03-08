0f2ade6820b90454559d6d0acced56da9090c5ba
Emmille Diogenes <emmille.diogenes@gmail.com>
2025-03-08 15:53:15 -0300

    Adicionada informações sobre itens na Página Categoria

--- src/pages/Private/Categorias/index.jsx
+++ src/pages/Private/Categorias/index.jsx
@@ -4,6 +4,31 @@ function Categorias() {
     return (
         <LayoutDefault>
             Categorias
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Calças</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Camisas</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Blusas</td>
+                </tr>
+                </tbody>
+                
+            </table>
+
+
         </LayoutDefault>
     )
 }
